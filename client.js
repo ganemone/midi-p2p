eb645756f00eb426f3cbd465b275692cfb1fdfc4
Giancarlo Anemone <ganemone@uber.com>
2020-06-09 17:39:58 -0400

    Allow for selecting midi in/out ports

--- client.js
+++ client.js
@@ -1,20 +1,46 @@
 const io = require('socket.io-client');
+const inquirer = require('inquirer');
 // Update this value based on your desired destination IP
-const socket = io('http://localhost:3000');
 const jazz = require('jazz-midi');
+const {Input, Output} = require('midi');
 const midi = new jazz.MIDI();
+const output = new Output();
 
-const out_name = midi.MidiOutOpen(0);
-
-if(!out_name){ 
-  throw new Error('Could not open out')
+// Count the available output ports.
+const outputPortCount = output.getPortCount();
+const outputOptions = ['System Default'];
+let j = 0;
+while (j < outputPortCount) {
+  outputOptions.push(output.getPortName(j));
+  j++;
 }
 
-socket.on('connect', () => {
-  console.log('Connected to server');
-  socket.on('midi', data => {
-    const [a,b,c] = data;
-    midi.MidiOut(a,b,c);
-  })
-})
-
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'port',
+    message: 'Select an output source',
+    choices: outputOptions.map((name, index) => {
+      return {
+        name, 
+        value: index,
+      }
+    })
+  }
+]).then((result) => {
+  const {port} = result;
+  const outName = midi.MidiOutOpen(port);
+  if(!outName) { 
+    throw new Error('Could not open out')
+  } else {
+    console.log(`Opened MidiOut connection to ${outName}`)
+  }
+  const socket = io('http://localhost:3000');
+  socket.on('connect', () => {
+    console.log('Connected to server');
+    socket.on('midi', data => {
+      const [a,b,c] = data;
+      midi.MidiOut(a,b,c);
+    });
+  });
+});
--- server.js
+++ server.js
@@ -1,6 +1,45 @@
 const jazz = require('jazz-midi');
-const midi = new jazz.MIDI();
+const inquirer = require('inquirer');
 const io = require('socket.io')();
+const midi = new jazz.MIDI();
+const {Input, Output} = require('midi');
+
+const input = new Input();
+ 
+// Count the available input ports.
+const inputPortCount = input.getPortCount();
+const inputOptions = [];
+let i = 0;
+while (i < inputPortCount) {
+  inputOptions.push(input.getPortName(i));
+  i++;
+}
+
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'port',
+    message: 'Select an input source',
+    choices: inputOptions.map((name, index) => {
+      return {
+        name, 
+        value: index,
+      }
+    })
+  }
+]).then((result) => {
+  const {port} = result;
+  const res = midi.MidiInOpen(0, function(t, msg){
+    io.emit('midi', msg);
+  });
+
+  if (!res) {
+    throw new Error(`Could not open MIDI-In port ${port}`);
+  } else {
+    console.log(`Opened MIDI-In connection to ${res}`);
+  }
+})
+
 io.on('connection', socket => { 
   console.log('Connected to client');
 });
@@ -9,11 +48,4 @@ io.on('connection', socket => {
 // The port is arbitrary and can be changed or set as an input
 io.listen(3000);
 
-const res = midi.MidiInOpen(0, function(t, msg){
-  io.emit('midi', msg);
-});
-
-if (!res) {
-  throw new Error('Could not open default MIDI-In port');
-}
 
